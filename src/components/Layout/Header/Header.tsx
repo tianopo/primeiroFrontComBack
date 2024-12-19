5736e957ddf9426057dce47a2210146b312a87e9
tianopo <matheuslink18@hotmail.com>
2024-12-19 12:42:43 -0300

    preço do dolar e bitcoin varia a cada 30 segundos no header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { Bell, Gear, List } from "@phosphor-icons/react";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { InputSearch } from "src/components/Form/Input/InputSearch";
 import { IconX } from "src/components/Icons/IconX";
 import { app } from "src/routes/app";
 import "../Sidebar/Sidebar.css";
@@ -17,11 +17,46 @@ interface IHeader {
   navbar: INavbar[];
 }
 
+interface ICurrencyData {
+  code: string;
+  bid: string; // Compra
+  ask: string; // Venda
+  high: string; // Máximo
+  low: string; // Mínimo
+  varBid: string; // Variação
+  pctChange: string; // Porcentagem de variação
+}
+
+interface ICurrencies {
+  USDBRL?: ICurrencyData;
+  BTCBRL?: ICurrencyData;
+}
+
 export const Header = ({ navbar }: IHeader) => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [currencyData, setCurrencyData] = useState<ICurrencies | null>(null);
   const navigate = useNavigate();
 
   const handleMenuToggle = () => setMenuOpen(!menuOpen);
+
+  useEffect(() => {
+    const fetchCurrencyData = async () => {
+      try {
+        const response = await axios.get("https://economia.awesomeapi.com.br/last/USD-BRL,BTC-BRL");
+        const data: ICurrencies = response.data;
+        setCurrencyData(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Erro ao buscar dados das moedas:", error);
+        setCurrencyData(null);
+      }
+    };
+
+    fetchCurrencyData();
+    const interval = setInterval(fetchCurrencyData, 30000); // Atualiza a cada 30 segundos
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
@@ -33,8 +68,33 @@ export const Header = ({ navbar }: IHeader) => {
           height={67.19}
           className="rounded-full border-1 border-edge-primary"
         />
-        <div className="w-full md:w-72">
-          <InputSearch title="pesquisar" placeholder="Pesquisar Projeto" />
+        <div className="w-full md:w-96">
+          <div className="flex flex-col items-start text-sm font-bold text-gray-700">
+            {currencyData ? (
+              <>
+                <span>Dólar (USD/BRL):</span>
+                <ul className="flex w-full flex-row flex-wrap gap-0.5 text-10 leading-tight tracking-tight text-green-500">
+                  <li>Compra R$ {parseFloat(currencyData.USDBRL?.bid || "0")} |</li>
+                  <li>Venda R$ {parseFloat(currencyData.USDBRL?.ask || "0")} |</li>
+                  <li>Máxima R$ {parseFloat(currencyData.USDBRL?.high || "0")} |</li>
+                  <li>Mínima R$ {parseFloat(currencyData.USDBRL?.low || "0")} |</li>
+                  <li>Variação R$ {currencyData.USDBRL?.varBid} |</li>
+                  <li>Porcentagem {currencyData.USDBRL?.pctChange}%</li>
+                </ul>
+                <span>Bitcoin (BTC/BRL):</span>
+                <ul className="flex flex-row flex-wrap gap-0.5 text-10 leading-tight tracking-tight text-green-500">
+                  <li>Compra R$ {parseFloat(currencyData.BTCBRL?.bid || "0")} |</li>
+                  <li>Venda R$ {parseFloat(currencyData.BTCBRL?.ask || "0")} |</li>
+                  <li>Máxima R$ {parseFloat(currencyData.BTCBRL?.high || "0")} |</li>
+                  <li>Mínima R$ {parseFloat(currencyData.BTCBRL?.low || "0")} |</li>
+                  <li>Variação R$ {currencyData.BTCBRL?.varBid} |</li>
+                  <li>Porcentagem {currencyData.BTCBRL?.pctChange}%</li>
+                </ul>
+              </>
+            ) : (
+              <span className="text-red-500">Erro ao carregar cotações</span>
+            )}
+          </div>
         </div>
         <div className="hidden items-center gap-2 md:flex">
           <IconX
