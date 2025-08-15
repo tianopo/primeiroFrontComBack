04278c1e38f08294b618096073a9e55128cce19e
tianopo <matheuslink18@hotmail.com>
2025-08-15 15:29:03 -0300

    aprofundando o código

--- src/utils/selectsOptions.ts
+++ src/utils/selectsOptions.ts
@@ -1,6 +1,7 @@
 export const exchangeOptions = [
   "Bybit https://www.bybit.com/ SG",
   "Binance https://www.binance.com/ CN",
+  "CRYPTOTECH https://www.cryptotechdev.com/ BR",
   "Huobi https://www.htx.com/ CN",
   "Kucoin https://www.kucoin.com/ SC",
   "CoinEx https://www.coinex.com/ HK",
